d8ddec42a5d328e92df8445333207a5ec7fe7f9a
Carlos Abraham <abraham@abranhe.com>
2018-10-10 11:42:48 -0400

    fix function typo on TestSorting

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -25,7 +25,7 @@ class TestSorting(unittest.TestCase):
         self.assertEqual([-44, 1, 2, 3, 7, 19], selection_sort([7, 3, 2, 19, -44, 1]))
 
     def test_pigeonhole_sort(self):
-        self.assertEqual([-44, 1, 2, 3, 7, 19], pidgeonhole_sort([7, 3, 2, 19, -44, 1]))
+        self.assertEqual([-44, 1, 2, 3, 7, 19], pigeonhole_sort([7, 3, 2, 19, -44, 1]))
 
     def test_stooge_sort(self):
         self.assertEqual([-44, 1, 2, 3, 7, 19], stooge_sort([7, 3, 2, 19, -44, 1]))
