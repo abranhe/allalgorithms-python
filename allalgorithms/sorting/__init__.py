46e5ecbaf411240d674a1573c161943087a0ca1e
Pratham1807 <35078777+Pratham1807@users.noreply.github.com>
2018-10-10 21:55:31 +0530

    Update __init__.py

--- allalgorithms/sorting/__init__.py
+++ allalgorithms/sorting/__init__.py
@@ -5,4 +5,4 @@ from .bubble_sort import bubble_sort
 from .pidgeonhole_sort import pidgeonhole_sort
 from .stooge_sort import stooge_sort
 from .cocktail_shaker_sort import cocktail_shaker_sort
-from .tree_sort import TreeSort
+from .tree_sort import tree_sort
